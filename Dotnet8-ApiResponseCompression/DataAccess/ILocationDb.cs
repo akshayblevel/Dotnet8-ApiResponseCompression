a895306ab9f5072fd3f0839ac46a1c38c030690e
Akshay Patel <akshayblevel@gmail.com>
2024-07-21 13:37:31 +0530

    Initial Commit

---
+++ Dotnet8-ApiResponseCompression/DataAccess/ILocationDb.cs
@@ -0,0 +1,12 @@
+﻿using Dotnet8_ApiResponseCompression.Interfaces;
+using Dotnet8_ApiResponseCompression.Models;
+
+namespace Dotnet8_ApiResponseCompression.DataAccess
+{
+    public interface ILocationDb
+    {
+        IRepository<Country> CountryRepository { get; }
+        IRepository<State> StateRepository { get; }
+        IRepository<City> CityRepository { get; }
+    }
+}
---
+++ Dotnet8-ApiResponseCompression/Program.cs
@@ -0,0 +1,52 @@
+using Dotnet8_ApiResponseCompression.DataAccess;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using System.IO.Compression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LocationContext>(opt => opt.UseSqlServer("server=DESKTOP-F22LNIE;database=Location;Trusted_Connection=True;TrustServerCertificate=True"));
+builder.Services.AddScoped<ILocationDb, LocationDb>();
+
+builder.Services.AddResponseCompression(options =>
+{
+    //options.EnableForHttps = true; 
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes;
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Optimal;
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.SmallestSize;
+});
+
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
